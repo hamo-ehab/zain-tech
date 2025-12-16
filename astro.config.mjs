01403fa25b7e9f4bef2f13a0d14ec69a8a2f4d08
hamo-ehab <zain@zain-tech.me>
2025-12-16 06:44:06 +0200

    Disable Wix to fix 500 Error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,60 +2,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import wix from "@wix/astro";
-import monitoring from "@wix/monitoring-astro";
 import react from "@astrojs/react";
-import sourceAttrsPlugin from "@wix/babel-plugin-jsx-source-attrs";
-import dynamicDataPlugin from "@wix/babel-plugin-jsx-dynamic-data";
 import customErrorOverlayPlugin from "./vite-error-overlay-plugin.js";
-
-const isBuild = process.env.NODE_ENV == "production";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   integrations: [
-    {
-      name: "framewire",
-      hooks: {
-        "astro:config:setup": ({ injectScript, command }) => {
-          if (command === "dev") {
-            injectScript(
-              "page",
-              `const version = new URLSearchParams(location.search).get('framewire');
-              if (version){
-                const localUrl = 'http://localhost:3202/framewire/index.mjs'\;
-                const cdnUrl = \`https://static.parastorage.com/services/framewire/\${version}/index.mjs\`;
-                const url = version === 'local' ? localUrl : cdnUrl;
-                const framewireModule = await import(url);
-                globalThis.framewire = framewireModule;
-                framewireModule.init({}, import.meta.hot);
-                console.log('Framewire initialized');
-              }`
-            );
-          }
-        },
-      },
-    },
     tailwind(),
-    wix({
-      htmlEmbeds: isBuild,
-      auth: true
-    }),
-    isBuild ? monitoring() : undefined,
-    react({ babel: { plugins: [sourceAttrsPlugin, dynamicDataPlugin] } }),
+    react(),
   ],
   vite: {
     plugins: [
       customErrorOverlayPlugin(),
     ],
-  },
-  devToolbar: {
-    enabled: false,
-  },
-  image: {
-    domains: ["static.wixstatic.com"],
   },
   server: {
     allowedHosts: true,
